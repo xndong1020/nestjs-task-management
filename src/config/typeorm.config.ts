6999c12784ed348034cee33d6a761e9d877a5e93
Jeremy Gu <jgu@networkten.com.au>
2019-11-15 13:11:55 +1100

    add typeorm, config typeorm for nestjs

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions, TypeOrmModule } from '@nestjs/typeorm'
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '1234',
+  database: 'tasks-management',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+}
